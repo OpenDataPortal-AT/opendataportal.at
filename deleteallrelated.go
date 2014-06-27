6c9b6185b91b7ee89046d1ea9767bc39f06d8228
Johann Höchtl <johann.hoechtl@gmail.com>
2014-06-27 23:47:12 +0200

    generalize

--- deleteallrelated.go
+++ deleteallrelated.go
@@ -1,7 +1,13 @@
+// Package deletes all "related" items in a CKAN instance. The portal has
+// to be specified via parameter "portal" and the neccessary CKAN-API key
+// has to be specified via environment variable CKANAUTH. The value of this
+// environment variable will be used as a value for X-CKAN-API-Key to identify
+// the user who will perform the API call.
 package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,33 +16,47 @@ import (
 	"strings"
 )
 
+const deleterelatedapicall = "api/3/action/related_delete"
+const relatedapicall = "api/3/action/related_list"
+
 func main() {
 	var related_json interface{}
-	var deleterelatedapiurl = "http://data.opendataportal.at/api/3/action/related_delete"
 
-	allrelated, err := http.Get("http://data.opendataportal.at/api/3/action/related_list")
+	portal := flag.String("portal", "", "url to CKAN portal, eg. http://data.opendataportal.at/")
+	flag.Parse()
+
+	if portal == nil || len(*portal) == 0 {
+		fmt.Println(`required parameter "portal" missing`)
+		os.Exit(1)
+	}
+
+	allrelated, err := http.Get(*portal + relatedapicall)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer allrelated.Body.Close()
+
 	body, err := ioutil.ReadAll(allrelated.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if err := json.Unmarshal(body, &related_json); err != nil {
 		log.Fatal(err)
 	}
 
 	container := related_json.(map[string]interface{})
 	results := container["result"].([]interface{})
+
 	auth := os.Getenv("CKANAUTH")
+
 	for i, v := range results {
 		result := v.(map[string]interface{})
 		id := result["id"].(string)
 
 		// Probalby unneccessary to append the id as an url parameter,
 		// as we are posting, but nice to see in the log
-		dl := fmt.Sprintf("%s?id=%s", deleterelatedapiurl, id)
+		dl := fmt.Sprintf("%s?id=%s", *portal+deleterelatedapicall, id)
 
 		fmt.Printf("[%d] Deleting related item with id %v\n", i, id)
 		id_json := fmt.Sprintf(`{"id": "%s"}`, id)
@@ -56,5 +76,9 @@ func main() {
 		respbytes, err := ioutil.ReadAll(resp.Body)
 		fmt.Printf("%s\n", string(respbytes))
 		resp.Body.Close()
+	}
+
+	if len(results) == 0 {
+		fmt.Println("Nothing to do")
 	}
 }
