b81104e5ad957502c718776debcd045796c50be5
Stefan Kasberger <stefan.kasberger@alpine-geckos.at>
2014-06-30 16:39:28 +0200

    bug fix: searchform, wrong name for php get

--- roots-opendataportalat/templates/searchform.php
+++ roots-opendataportalat/templates/searchform.php
@@ -6,9 +6,9 @@
 */
 ?>
 
-<form role="search" method="get" class="search-form search-form-header form-inline" action="<?php echo get_template_directory_uri(); ?>/ckan-search.php" data-ckan="<?php echo get_template_directory_uri(); ?>/ckan-search.php" data-wordpress="<?php echo home_url('/'); ?>">
+<form role="search" method="get" class="search-form search-form-header form-inline" action="<?php echo get_template_directory_uri(); ?>/ckan-search.php/?q" data-ckan="<?php echo get_template_directory_uri(); ?>/ckan-search.php" data-wordpress="<?php echo home_url('/'); ?>">
   <div class="input-group">
-    <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" class="search-field row form-control" placeholder="Suchbegriff eingeben">
+    <input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="q" class="search-field row form-control" placeholder="Suchbegriff eingeben">
     <label class="hide"><?php _e('Search for:', 'roots'); ?></label>
     <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default"></button>
