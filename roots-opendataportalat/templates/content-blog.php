326da354290fb7c51007d9884757f4aab100bdde
Stefan Kasberger <mail@stefankasberger.at>
2015-03-01 22:38:39 +0100

    fix pagination bug: missed 2 files in last commit

---
+++ roots-opendataportalat/templates/content-blog.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Preview of news rendered in index.php, front-page.php and template-thema.php.
+*/
+?>
+
+<div class="header row">
+	<div class="preview-date col-md-1">
+		<?php get_template_part('templates/content', 'date-field'); ?>
+	</div>
+	<div class="col-md-11">
+		<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+
+		<?php get_template_part('templates/news-meta'); ?>
+		<div class="entry-summary">
+			<div class="entry-content preview-excerpt">
+				<?php the_excerpt(); ?>
+				<strong><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">weiterlesen</a></strong>
+			</div>
+		</div>
+	</div>
+</div>
+
+
