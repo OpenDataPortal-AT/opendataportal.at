28cc4a5c72169948b0b2659ed238c93d7dc4d247
Wisnu Adi Nurcahyo <wisnu@nurcahyo.me>
2016-11-02 23:11:10 +0700

    Use DRY Concept For Better Reading

--- roots-opendataportalat/functions.php
+++ roots-opendataportalat/functions.php
@@ -2,18 +2,25 @@
 /**
  * Roots includes
  */
-require_once locate_template('/lib/utils.php');           // Utility functions
-require_once locate_template('/lib/init.php');            // Initial theme setup and constants
-require_once locate_template('/lib/wrapper.php');         // Theme wrapper class
-require_once locate_template('/lib/sidebar.php');         // Sidebar class
-require_once locate_template('/lib/config.php');          // Configuration
-require_once locate_template('/lib/activation.php');      // Theme activation
-require_once locate_template('/lib/titles.php');          // Page titles
-require_once locate_template('/lib/cleanup.php');         // Cleanup
-require_once locate_template('/lib/nav.php');             // Custom nav modifications
-require_once locate_template('/lib/gallery.php');         // Custom [gallery] modifications
-require_once locate_template('/lib/comments.php');        // Custom comments modifications
-require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
-require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
-require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
-require_once locate_template('/lib/custom.php');          // Custom functions
+
+ $libs = [
+   'utils',             // Utility functions
+   'init',              // Initial theme setup and constants
+   'wrapper',           // Theme wrapper class
+   'sidebar',           // Sidebar class
+   'config',            // Configuration
+   'activation',        // Theme activation
+   'titles',            // Page titles
+   'cleanup',           // Cleanup
+   'nav',               // Custom nav modifications
+   'gallery',           // Custom [gallery] modifications
+   'comments',          // Custom comments modifications
+   'relative-urls',     // Root relative URLs
+   'widgets',           // Sidebars and widgets
+   'scripts',           // Scripts and stylesheets
+   'custom'             // Custom functions
+ ];
+
+ foreach ($libs as $lib) {
+   require_once locate_template('/lib/' . $lib . '.php');
+ }
